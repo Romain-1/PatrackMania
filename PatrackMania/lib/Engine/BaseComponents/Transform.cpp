4acc7e3255f58cf859959193a8e32f378bb22ad2
Romain-1 <romain.minguet@epitech.eu>
2023-06-02 23:18:27 +0900

    fix transform shit

--- PatrackMania/lib/Engine/BaseComponents/Transform.cpp
+++ PatrackMania/lib/Engine/BaseComponents/Transform.cpp
@@ -75,9 +75,69 @@ glm::quat FromToRotation(glm::vec3 aFrom, glm::vec3 aTo)
 	return glm::angleAxis(angle, glm::normalize(axis));
 }
 
-void Transform::SetUp(const glm::vec3& up) { m_localRotation = FromToRotation(glm::vec3(0, 1, 0), up);  RecalculateFromLocal(); }
-void Transform::SetRight(const glm::vec3& right) { m_localRotation = FromToRotation(glm::vec3(1, 0, 0), right);  RecalculateFromLocal(); }
-void Transform::SetForward(const glm::vec3& forward) { m_localRotation = FromToRotation(glm::vec3(0, 0, 1), forward);  RecalculateFromLocal(); }
+glm::quat LookRotation(glm::vec3 forward, glm::vec3 upwards)
+{
+	forward = glm::normalize(forward);
+
+	glm::vec3 vector = glm::normalize(forward);
+	glm::vec3 vector2 = glm::normalize(glm::cross(upwards, vector));
+	glm::vec3 vector3 = glm::cross(vector, vector2);
+	auto m00 = vector2.x;
+	auto m01 = vector2.y;
+	auto m02 = vector2.z;
+	auto m10 = vector3.x;
+	auto m11 = vector3.y;
+	auto m12 = vector3.z;
+	auto m20 = vector.x;
+	auto m21 = vector.y;
+	auto m22 = vector.z;
+
+
+	float num8 = (m00 + m11) + m22;
+	glm::quat quaternion(1, 0, 0, 0);
+
+	if (num8 > 0.f)
+	{
+		auto num = (float)std::sqrt(num8 + 1.f);
+		quaternion.w = num * 0.5f;
+		num = 0.5f / num;
+		quaternion.x = (m12 - m21) * num;
+		quaternion.y = (m20 - m02) * num;
+		quaternion.z = (m01 - m10) * num;
+		return quaternion;
+	}
+	if ((m00 >= m11) && (m00 >= m22))
+	{
+		auto num7 = (float)std::sqrt(((1.f + m00) - m11) - m22);
+		auto num4 = 0.5f / num7;
+		quaternion.x = 0.5f * num7;
+		quaternion.y = (m01 + m10) * num4;
+		quaternion.z = (m02 + m20) * num4;
+		quaternion.w = (m12 - m21) * num4;
+		return quaternion;
+	}
+	if (m11 > m22)
+	{
+		auto num6 = (float)std::sqrt(((1.f + m11) - m00) - m22);
+		auto num3 = 0.5f / num6;
+		quaternion.x = (m10 + m01) * num3;
+		quaternion.y = 0.5f * num6;
+		quaternion.z = (m21 + m12) * num3;
+		quaternion.w = (m20 - m02) * num3;
+		return quaternion;
+	}
+	auto num5 = (float)std::sqrt(((1.f + m22) - m00) - m11);
+	auto num2 = 0.5f / num5;
+	quaternion.x = (m20 + m02) * num2;
+	quaternion.y = (m21 + m12) * num2;
+	quaternion.z = 0.5f * num5;
+	quaternion.w = (m01 - m10) * num2;
+	return quaternion;
+}
+
+void Transform::SetUp(const glm::vec3& up) { m_localRotation = LookRotation(glm::vec3(0, 1, 0), up);  RecalculateFromLocal(); }
+void Transform::SetRight(const glm::vec3& right) { m_localRotation = LookRotation(glm::vec3(1, 0, 0), right);  RecalculateFromLocal(); }
+void Transform::SetForward(const glm::vec3& forward) { m_localRotation = LookRotation(forward, glm::vec3(0, 1, 0));  RecalculateFromLocal(); }
 
 void Transform::SetParent(Transform* parent)
 {
